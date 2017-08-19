6969770ad5cd1e1b18bdc73ff1fa9cd2123af58d
cbryant24 <chrs.bryant@gmail.com>
2017-08-18 17:34:11 -0700

    sgt v2.0 - chris bryant

--- script.js
+++ script.js
@@ -78,23 +78,29 @@ $(document).ready(function(){
     $('#studentGrade').val('90');
     $('#studentName').val('chris');
     $('#course').val('math');
+    sgt.load_students();
 })
 
 function SGT() {
     var name = '';
     var student_course = '';
     var student_grade = null;
-    var self = this
+    this.student_name = '';
+    this.student_grade = null;
+    this.student_course = '';
     this.student_id = null;
     this.student_list = [];
-    $('.btn-success').on('click', ()=>{
-        name = $('#studentName').val()
-        student_course =$('#course').val();
-        student_grade = $('#studentGrade').val();
-        this.add_student();
-        $('.avgGrade').text(this.update_list_average());
-        this.add_student_dom();
-    });
+    $('.btn-success').on('click', (function(sgt){
+        return function(){
+            sgt.student_name = $('#studentName').val();
+            sgt.student_course =$('#course').val();
+            sgt.student_grade = $('#studentGrade').val();
+            sgt.add_student();
+            $('.avgGrade').text(sgt.update_average());
+            sgt.add_student_dom();
+            sgt.send_student();
+        }
+    })(this));
 
     $('.cancel').on('click', function() {
         $('#studentName').val('');
@@ -102,36 +108,27 @@ function SGT() {
         $('#studentGrade').val('');
     });
 
-    $('.server').on('click', function(){
-        var key = 'api_key: 5ukJMiUivX'
-        $.ajax({
-            url: 'http://s-apis.learningfuze.com/sgt/get',
-            data: {
-                api_key: '5ukJMiUivX'
-            },
-            method: 'post',
-            dataType: 'json',
-            success: function(response) {
-                debugger;
-                for (var i = 0; i < response.data.length; i++) {
-                    self.student_list.push(response.data[i]);
-                    self.add_student_dom();
-                }
-            }
-        })
-    })
+    $('.server').on('click', (function(sgt){
+        $('tbody').empty();
+        return function(){return sgt.load_students()}
+    })(this));
 
+    $('.modal-footer').on('click', 'button',function(){
+        $('#myModal .modal-body').empty();
+        debugger
+        $('.btn-danger span').removeClass('fa fa-circle-o-notch fa-spin');
+    });
     this.add_student = ()=>{
         this.student = {
-            id: this.student_id,
-            name: name,
-            course: student_course,
-            grade: student_grade
+            id: this.student_list.length <= 0 ? 0:this.student_list[this.student_list.length - 1].id + 1,
+            name: this.student_name,
+            grade: parseInt(this.student_grade),
+            course: this.student_course
         };
         this.student_list.push(this.student);
     };
 
-    this.update_list_average = function() {
+    this.update_average = function() {
         this.average = null;
         for(var i = 0; i < this.student_list.length; i++) {
             this.average += parseInt(this.student_list[i].grade)
@@ -140,9 +137,10 @@ function SGT() {
     };
 
     this.add_student_dom = function() {
-        for(var i = this.student_list.length - 1; i <  this.student_list.length; i++) {
+        $('tbody').empty();
+        for(var i = 0; i <  this.student_list.length; i++) {
             var table_row = $('<tr>', {
-                id: this.student_id++
+                id: this.student_list[i].student_id
             });
             var table_name = $('<td>', {
                 text: this.student_list[i].name
@@ -156,34 +154,126 @@ function SGT() {
             var table_delete = $('<td>',{
 
             });
-            // var vJSbutton = document.createElement('BUTTON');
-            // vJSbutton.classList.add('btn');
-            // vJSbutton.classList.add('btn-danger');
-            // vJSbutton.innerText = 'Delete';
-            var jQueryButton = $('<button>', {
+            var delete_button = $('<button>', {
                 class: "btn btn-danger",
-                text: 'Delete'
+                text: 'Delete  '
             });
-            var delete_info = jQueryButton[0];
+            var delete_info = delete_button[0];
             delete_info.student = this.student_list[i];
-            // var vanillaJSButton = jQueryButton[0];
-            // vanillaJSButton.student = this.student_list[i];
-            // table_delete.append(vanillaJSButton);
             table_row.append(table_name);
             table_row.append(table_course);
             table_row.append(table_grade);
             table_row.append(table_delete);
-            table_delete.append(jQueryButton);
+            table_delete.append(delete_button);
             $('.student-list').append(table_row);
+            this.handle_delete(delete_button);
         }
-        $('tr').on('click','.btn-danger',function() {
-            debugger
-            // console.log($(this).parent('tr').attr('id'))
-            // console.log(this.student);
-            self.student_list.splice(self.student_list.indexOf($(this.student),1));
-            $(this).parents('tr').remove();
+    };
 
-        });
+    this.handle_delete = function(element) {
+        $(element).on('click',(function(sgt){return function() {
+            $(this).off('click');
+            var $span_add = $('<span>', {
+                class: "fa fa-circle-o-notch fa-spin",
+            });
+            debugger;
+            $(this).append($span_add);
+            var the_data = {
+                api_key: '5ukJMiUivX',
+                student_id: this.student.id
+            };
+            $.ajax({
+                method: 'post',
+                url: 'http://s-apis.learningfuze.com/sgt/delete',
+                data: the_data,
+                dataType: 'json',
+                success: (function(btn){return function(response) {
+                    if(response.success === true) {
+                        console.log('success!');
+                        sgt.student_list.splice(sgt.student_list.indexOf($(sgt.student),1));
+                        $(btn).parents('tr').remove();
+                    } else {
+                        for(var i = 0; i < response.errors.length; i++) {
+                            var $error_message = $('<p>',{
+                                text: response.errors[i]
+                            });
+                            $('#myModal .modal-body').append($error_message);
+
+                        }
+                        $(btn).on('click', sgt.handle_delete(btn));
+                        $("#myModal").modal();
+                    }
+                }})(this),
+                error: function(response){
+                    console.log(response)
+                }
+            })
+        }}(this)));
+    }
+
+    this.load_students = (function(sgt){return function() {
+        $.ajax({
+            url: 'http://s-apis.learningfuze.com/sgt/get',
+            data: {
+                api_key: '5ukJMiUivX'
+            },
+            method: 'post',
+            dataType: 'json',
+            success: function(response) {
+                if (response.success === true) {
+                    for (var i = 0; i < response.data.length; i++) {
+                        sgt.student_list.push(response.data[i]);
+                        sgt.add_student_dom();
+                        $('.avgGrade').text(sgt.update_average());
+                    }
+                } else {
+                    var $error_message = $('<p>',{
+                        text: response.errors
+                    });
+                    $('#myModal .modal-body').append($error_message);
+
+                    $("#myModal").modal();
+                    console.log(response.errors[0])
+                }
+            }, error: function(response) {
+                console.log(response)
+            }
+        })
+    }})(this);
+
+    this.send_student = function() {
+        var the_data = {
+            api_key: '5ukJMiUivX',
+            id: null,
+            name: this.student_name,
+            course: this.student_course,
+            grade: this.student_grade
+        };
+        the_data.id = this.student.id;
+        $.ajax({
+            url: 'http://s-apis.learningfuze.com/sgt/create',
+            method: 'post',
+            data: the_data,
+            dataType: 'json',
+            success: function(response) {
+                if(response.success === true) {
+                    console.log('the push worked, ', response.success);
+                    console.log('the new id number is ', response.new_id)
+                } else {
+                    for(var i = 0; i < response.errors.length; i++) {
+                        var $error_message = $('<p>',{
+                            text: response.errors[i]
+                        });
+                        $('#myModal .modal-body').append($error_message);
+
+                    }
+                    $("#myModal").modal();
+                    console.log(response.errors[0])
+                }
+            }, error: function(response) {
+                console.log(response)
+            }
+        })
     }
 }
 
